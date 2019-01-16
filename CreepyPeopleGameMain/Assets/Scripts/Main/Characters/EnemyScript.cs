95d2ed5efd3104b3730d9ac4a61d9d122d2def6f
KentPhan <kent.chi.lam@gmail.com>
2019-01-16 14:02:25 -0700

    Added flashlight stun

--- CreepyPeopleGameMain/Assets/Scripts/Main/Characters/EnemyScript.cs
+++ CreepyPeopleGameMain/Assets/Scripts/Main/Characters/EnemyScript.cs
@@ -1,17 +1,26 @@
-﻿using Assets.Scripts.Main.Managers;
+using System;
+using Assets.Scripts.Main.Managers;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace Assets.Scripts.Main.Characters
 {
+    public enum EnemyStates
+    {
+        ACTIVE,
+        STUNNED
+    }
+
     [RequireComponent(typeof(NavMeshAgent))]
     public class EnemyScript : MonoBehaviour
     {
         //[SerializeField] private float EnemyRange = 30.0f;
-        [SerializeField] private float StunTime = 5.0f;
 
         private PlayerScript m_Player;
         private NavMeshAgent m_Agent;
+        private EnemyStates m_CurrentState;
+        private float m_CurrentStunTime;
+
 
         // Start is called before the first frame update
         void Start()
@@ -23,16 +32,34 @@ namespace Assets.Scripts.Main.Characters
         // Update is called once per frame
         void Update()
         {
-            if (m_Player != null)
+            switch (m_CurrentState)
             {
-                // Calculate Range from Player
-
-                m_Agent.destination = m_Player.transform.position;
+                case EnemyStates.ACTIVE when m_Player != null:
+                    // Calculate Range from Player
+                    m_Agent.destination = m_Player.transform.position;
+                    break;
+                case EnemyStates.ACTIVE:
+                    m_Player = GameManager.Instance.GetCurrentPlayer();
+                    break;
+                case EnemyStates.STUNNED:
+                    m_CurrentStunTime -= Time.deltaTime;
+                    if (m_CurrentStunTime <= 0.0f)
+                    {
+                        m_CurrentState = EnemyStates.ACTIVE;
+                        m_Agent.enabled = true;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
-            else
-            {
-                m_Player = GameManager.Instance.GetCurrentPlayer();
-            }
+        }
+        public void Stun(float i_StunTime)
+        {
+            m_CurrentState = EnemyStates.STUNNED;
+            m_CurrentStunTime = i_StunTime;
+            m_Agent.enabled = false;
+            return;
         }
     }
 }
+
--- CreepyPeopleGameMain/Assets/Scripts/Main/Characters/PlayerScript.cs
+++ CreepyPeopleGameMain/Assets/Scripts/Main/Characters/PlayerScript.cs
@@ -7,10 +7,17 @@ namespace Assets.Scripts.Main.Characters
         [SerializeField]
         private Light FlashLight;
 
+        [SerializeField]
+        private float FlashLightStunTime = 5.0f;
+
+        private float FlashLightStunRange = 20.0f;
+        private int m_EnemyMask;
+
         // Start is called before the first frame update
         void Start()
         {
             FlashLight.enabled = false;
+            m_EnemyMask = LayerMask.GetMask("Enemy");
         }
 
         // Update is called once per frame
@@ -21,8 +28,16 @@ namespace Assets.Scripts.Main.Characters
                 FlashLight.enabled = !FlashLight.enabled;
             }
 
+            // If Flashlight hits enemies
             if (FlashLight.enabled)
             {
+                RaycastHit l_hitInfo;
+                Ray l_FlashlightRay = new Ray(transform.position, transform.forward);
+                if (Physics.Raycast(l_FlashlightRay, out l_hitInfo, FlashLightStunRange, m_EnemyMask,
+                    QueryTriggerInteraction.Collide))
+                {
+                    l_hitInfo.collider.GetComponent<EnemyScript>().Stun(FlashLightStunTime);
+                }
                 // Put logic for flashlight stunning
             }
         }
@@ -36,7 +51,5 @@ namespace Assets.Scripts.Main.Characters
         {
             FlashLight.enabled = false;
         }
-
-
     }
 }
